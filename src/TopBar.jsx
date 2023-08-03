6d052ca083c9e0292c8ae138f044e7a7ed63f1a6
Karthik Chandrasekhar <karthikchandrasekhar03@outlook.com>
2023-08-03 10:08:20 +0530

    Added status indicators to topbar icons

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -5,6 +5,8 @@ import messageIcon from './assets/Icons/TopBar/Message.png'
 import settingsIcon from './assets/Icons/TopBar/Settings.png'
 import userIcon from './assets/Icons/Components/user.png'
 import statusGreenIcon from './assets/Icons/TopBar/statusGreen.png'
+import statusGreyIcon from './assets/Icons/TopBar/statusGrey.png'
+import statusRedIcon from './assets/Icons/TopBar/statusRed.png'
 const TopBar = (props) => {
     return (
         <div class="topbar">
@@ -13,11 +15,19 @@ const TopBar = (props) => {
                 <input type="text" placeholder="Search here" className="searchArea"></input>
             </div> 
 
-            <div>
-                <div className="topBarStatusIcon"><img src={alertIcon}></img></div>
+            <div style={{position:'relative'}}>
+                <div className="topBarStatusIcon"><img src={alertIcon} style={{position:'relative'}}></img></div>
+                <img src={statusGreenIcon} style={{position:'absolute', zIndex:1, top:'0px', right:'0px', marginLeft:'100px'}}/>
             </div>
-            <div className="topBarStatusIcon"><img src={messageIcon}></img></div>
-            <div className="topBarStatusIcon"><img src={settingsIcon}></img></div>
+            <div style={{position:'relative'}}>
+                <div className="topBarStatusIcon"><img src={messageIcon} style={{position:'relative'}}></img></div>
+                <img src={statusRedIcon} style={{position:'absolute', zIndex:1, top:'0px', right:'0px', marginLeft:'100px'}}/>
+            </div>
+            <div style={{position:'relative'}}>
+                <div className="topBarStatusIcon"><img src={settingsIcon} style={{position:'relative'}}></img></div>
+                <img src={statusGreyIcon} style={{position:'absolute', zIndex:1, top:'0px', right:'0px', marginLeft:'100px'}}/>
+            </div>
+            
             <p style={{fontFamily:'Poppins-Bold'}}>User</p>
             <div className="topBarStatusIcon"><img src={userIcon} style={{height:'60px', width:'60px'}}></img></div>
         </div>
