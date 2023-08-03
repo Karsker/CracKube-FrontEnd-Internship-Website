504b7fb0b058ad726be88d17d7c4f9bb2b2c6637
Karthik Chandrasekhar <karthikchandrasekhar03@outlook.com>
2023-08-03 12:31:50 +0530

    Set focus on dashboard link when page loads

--- src/SideNav.jsx
+++ src/SideNav.jsx
@@ -7,12 +7,18 @@ import spendingIcon from './assets/Icons/SideNavbar/Spending.png'
 import formCampaignIcon from './assets/Icons/SideNavbar/FormCampaign.png'
 import analyticsIcon from './assets/Icons/SideNavbar/Analytics.png'
 import notificationIcon from './assets/Icons/SideNavbar/Notification.png'
+import { useRef, useEffect } from 'react'
 const SideNav = (props) => {
+    const inputReference = useRef(null);
+
+    useEffect(() => {
+        inputReference.current.focus();
+    }, []);
     return (
         <div className="sidenav">
             <div class="navlogo"><img src={headerLogo} style={{height: 35, width: 41}}/>Iklan.</div>
 
-            <div className="navbarLink"><img src={homeIcon} className="navIcon"/><a href="#section">Dashboard</a><div className="selectionBar"></div></div>
+            <div className="navbarLink" ><img src={homeIcon} className="navIcon"/><a href="#section" id="home" ref={inputReference}>Dashboard</a><div className="selectionBar"></div></div>
             <div className="navbarLink"><img src={categoryIcon} className="navIcon"/><a href="#section">Campaign</a><div className="selectionBar"></div></div>
             <div className="navbarLink"><img src={socialMediaIcon} className="navIcon"/><a href="#section">Social Network</a><div className="selectionBar"></div></div>
             <div className="navbarLink"><img src={spendingIcon} className="navIcon"/><a href="#section">Spending</a><div className="selectionBar"></div></div>
@@ -20,7 +26,9 @@ const SideNav = (props) => {
             <div className="navbarLink"><img src={formCampaignIcon} className="navIcon"/><a href="#section">Form Campaign</a><div className="selectionBar"></div></div>
             <div className="navbarLink"><img src={notificationIcon} className="navIcon"/><a href="#section">Notification</a><div className="selectionBar"></div></div>
             <div className="navbarLink"><img src={homeIcon} className="navIcon"/><a href="#section">Payment</a><div className="selectionBar"></div></div>
+            
         </div>
+        
     )
 }
 
